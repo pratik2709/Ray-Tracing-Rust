d8bb224d33b793dd3e7d33c6aff8a7aa94e33bea
pratiksaxena <pratik2709@gmail.com>
2019-03-16 00:18:57 +0530

    added file writing utilities

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::io::Write;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
 fn main(){
+
+    let actual_path = Path::new("output.txt");
+    let mut actual_file = match File::create(&actual_path) {
+        Err(why) => panic!("Something went wrong:: {}", why.description()),
+        Ok(actual_file) => actual_file,
+    };
+
     let nx = 200;
     let ny = 100;
-    println!("{}{} {} {}", "P3\n", nx , ny , "\n255\n");
+    let first_line = format!("{}{} {} {}", "P3\n", nx , ny , "\n255\n");
+    actual_file.write(first_line.as_bytes());
 
     let mut j = ny-1;
     while j>= 0 {
@@ -15,7 +28,8 @@ fn main(){
             let ir = (255.99 * r) as i32;
             let ig = (255.99 * g) as i32;
             let ib = (255.99 * b) as i32;
-            println!("{} {} {} \n", ir,ig, ib);
+            let new_string = format!("{} {} {} \n", ir,ig, ib);
+            actual_file.write(new_string.as_bytes());
 
         }
 
