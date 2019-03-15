66195d484c42ac6220a195646b81afa402dda53f
pratiksaxena <pratik2709@gmail.com>
2019-03-15 23:10:49 +0530

    intermediate for raytracer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let nx = 200;
+    let ny = 100;
+    println!("{}{} {} {}", "P3\n", nx , ny , "\n255\n");
+    for j in ny-1..0 {
+        println!("{}", j)
+    }
+
 }
