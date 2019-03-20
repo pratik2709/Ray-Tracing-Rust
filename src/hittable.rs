d2dc3209b5883f73731632b6dac3f0153e5083ad
pratiksaxena <pratik2709@gmail.com>
2019-03-21 00:30:27 +0530

    hit method for sphere

--- src/hittable.rs
+++ src/hittable.rs
@@ -13,9 +13,19 @@ struct sphere{
     radius: f32,
 }
 
+impl sphere{
+    fn center(&self) -> Vec3{
+        Vec3 {
+            x:self.center.x,
+            y:self.center.y,
+            z:self.center.z,
+        }
+    }
+}
+
 impl RayHit for sphere{
     fn hit(self, ray: Ray, t_min:f32, t_max: f32, mut rec: hit_record) -> bool{
-        let oc = ray.getOrigin() - self.center;
+        let oc = ray.getOrigin() - self.center();
         let a:f32 = ray.getDirection().dot(&ray.direction);
         let b:f32 = ray.getDirection().dot(&oc);
         let c:f32 = oc.dot(&oc) - self.radius*self.radius;
@@ -25,15 +35,15 @@ impl RayHit for sphere{
             if temp < t_max && temp > t_min {
                 rec.t = temp;
                 rec.p = ray.point_at_parameter(rec.t);
-                rec.normal = (rec.p - center)/radius;
-                true
+                rec.normal = (rec.p - self.center())/self.radius;
+                return true
             }
             temp = (-b + (b*b-a*c).sqrt())/a;
             if temp < t_max && temp > t_min {
                 rec.t = temp;
                 rec.p = ray.point_at_parameter(rec.t);
-                rec.normal = (rec.p - center)/radius;
-                true
+                rec.normal = (rec.p - self.center())/self.radius;
+                return true
             }
         }
         false
